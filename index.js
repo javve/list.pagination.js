32117cb0b2052832f59435fcaa424cff721c6a6a
Jonny Strömberg <jonny.stromberg@gmail.com>
2013-11-14 12:03:31 +0100

    fix jshint errors

--- index.js
+++ index.js
@@ -8,15 +8,17 @@ module.exports = function(options) {
         list;
 
     var refresh = function() {
-        var l = list.matchingItems.length,
+        var item,
+            l = list.matchingItems.length,
             index = list.i,
             page = list.page,
             pages = Math.ceil(l / page),
             currentPage = Math.ceil((index / page)),
             innerWindow = options.innerWindow || 2,
             left = options.left || options.outerWindow || 0,
-            right = options.right || options.outerWindow || 0,
-            right = pages - right;
+            right = options.right || options.outerWindow || 0;
+
+        right = pages - right;
 
         pagingList.clear();
         for (var i = 1; i <= pages; i++) {
@@ -25,7 +27,7 @@ module.exports = function(options) {
             //console.log(i, left, right, currentPage, (currentPage - innerWindow), (currentPage + innerWindow), className);
 
             if (is.number(i, left, right, currentPage, innerWindow)) {
-                var item = pagingList.add({
+                item = pagingList.add({
                     page: i,
                     dotted: false
                 })[0];
@@ -34,7 +36,7 @@ module.exports = function(options) {
                 }
                 addEvent(item.elm, i, page);
             } else if (is.dotted(i, left, right, currentPage, innerWindow, pagingList.size())) {
-                var item = pagingList.add({
+                item = pagingList.add({
                     page: "...",
                     dotted: true
                 })[0];
@@ -57,17 +59,16 @@ module.exports = function(options) {
             return ( i >= (currentPage - innerWindow) && i <= (currentPage + innerWindow));
         },
         dotted: function(i, left, right, currentPage, innerWindow, currentPageItem) {
-            return this.dottedLeft(i, left, right, currentPage, innerWindow)
-            || (this.dottedRight(i, left, right, currentPage, innerWindow, currentPageItem));
+            return this.dottedLeft(i, left, right, currentPage, innerWindow) || (this.dottedRight(i, left, right, currentPage, innerWindow, currentPageItem));
         },
         dottedLeft: function(i, left, right, currentPage, innerWindow) {
-            return ((i == (left + 1)) && !this.innerWindow(i, currentPage, innerWindow) && !this.right(i, right))
+            return ((i == (left + 1)) && !this.innerWindow(i, currentPage, innerWindow) && !this.right(i, right));
         },
         dottedRight: function(i, left, right, currentPage, innerWindow, currentPageItem) {
             if (pagingList.items[currentPageItem-1].values().dotted) {
-                return false
+                return false;
             } else {
-                return ((i == (right)) && !this.innerWindow(i, currentPage, innerWindow) && !this.right(i, right))
+                return ((i == (right)) && !this.innerWindow(i, currentPage, innerWindow) && !this.right(i, right));
             }
         }
     };
